44aba3b01169771ea2ad6eb342ba2fdb0bd7ea76
Ian <itmacleod@email.wm.edu>
2022-04-12 09:45:53 -0400

    Added comments to battleship class

--- battleship.py
+++ battleship.py
@@ -1,6 +1,23 @@
 from ship import Ship
 
+
 class Battleship(Ship):
+    """
+    A class used to represent a battleship, which inherites from the Ship class
+
+    ...
+
+    name : str
+        name of ship as a single captial letter (i.e "B" for battleship)
+    spaces: int
+        how many spaces on the board the ship takes up
+
+    """
+
     def __init__(self, name):
+        """
+        Constructs the battleship object
+        :param name: name of ship (i.e B for battleship)
+        """
         super().__init__(name)
         self.spaces = 4
